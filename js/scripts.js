e71f1ccb20224c239575f3d7814fc5bca53ade9e
Imma7 <immamugambi@gmail.com>
2018-04-09 04:55:55 +0300

    Introduce hide feature after clicking start

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+// Business Logic
+var player1 = "";
+var player2 = "";
+
+var randomNum = function() {
+    return Math.floor(6*Math.random())+1;
+}
+
+function Player(turn) {
+    this.roll = 0;
+    this.tempscore = 0;
+    this.totalscore = 0;
+    this.turn = 0;
+    this.playerName;
+}
+
+// Checking for 1
+Player.prototype.rollOne = function() {
+    if (this.roll === 1) {
+        this.tempscore = 0;
+        alert("Sorry" + this.playerName + " you got a 1. Your turn is over!")
+        // change turn
+    } else {
+        this.tempscore += this.roll;
+    }
+}
+
+
+// User Interface Logic
+$(document).ready(function(){
+    $("button#start").click(function(event) {
+      player1 = new Player(true);
+      player2 = new Player(false);
+
+      $(".container").hide();
+
+      var player1Name = $(".player1Name").val();
+      ("#player1Name").text(player1Name);
+
+      var player2Name = $(".player2Name").val();
+      ("#player2Name").text(player2Name);
+
+      player1.playerName = player1Name;
+      player2.playerName = player2Name;
+
+      
+
+    $("button#p1Roll").click(function(event) {
+        p1.roll = randomNum();
+        $("#rollDie1").text(p1.roll);
+        p1.rollOne();
+        $("#score1").text(player1.tempscore);
+
+    });
+
+    });
+});
